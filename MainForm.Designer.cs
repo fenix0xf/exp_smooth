04052086e3e5086c4486a079261fba5b7cfb3fa8
Alexander Kotikov <alexandr.kotikov@gmail.com>
2018-01-10 19:08:01 +0300

    Initial commit 2

---
+++ MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(974, 570);
+            this.panel1.TabIndex = 1;
+            this.panel1.SizeChanged += new System.EventHandler(this.Panel1_SizeChanged);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(12, 599);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(974, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Smoothing factor (0 < a < 1):";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(12, 627);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(974, 58);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 697);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exponential Smoothing";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class MainForm : Form
+    {
+        private Graphics _gfx;
+        private Pen _penArr = new Pen(Color.Silver);
+        private Pen _penExp = new Pen(Color.Red);
+        private Pen _penSine = new Pen(Color.Green);
+        private Font _font = new Font(FontFamily.GenericSansSerif, 12);
+        private int[] _arr;
+        private int[] _arrExp;
+        private int[] _arrSine;
+        private Size _panelSize;
+        private double _smoothFact = 0.01; ///< Smoothing factor (0 < a < 1).
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _panelSize = panel1.Size;
+        }
+
+        private void PanelClear()
+        {
+            _gfx.Clear(Color.White);
+        }
+
+        private void PanelDrawArray<T>(Pen pen, IList<T> arr) where T: IConvertible
+        {
+            int zero = panel1.Size.Height / 2;
+
+            var points = new Point[arr.Count];
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                points[i].X = i;
+                points[i].Y = arr[i].ToInt32(NumberFormatInfo.CurrentInfo) + zero;
+            }
+
+            _gfx.DrawLines(pen, points);
+        }
+
+        private void Draw()
+        {
+            /// Updating size of the panel.
+            if ((_gfx == null) || (_panelSize != panel1.Size))
+            {
+                _panelSize = panel1.Size;
+
+                if (_gfx != null)
+                {
+                    _gfx.Dispose();
+                }
+
+                _gfx = panel1.CreateGraphics();
+            }
+
+            var rnd = new Random();
+            int maxValue = panel1.Height / 4;
+            
+            int exp = 0;
+
+            _arr = new int[panel1.Size.Width];
+            _arrExp = new int[panel1.Size.Width];
+            _arrSine = new int[panel1.Size.Width];
+
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                int noise = rnd.Next(150);
+
+                var angle = Math.PI * i / 180 / 2;
+                var sv = (int)(Math.Sin(angle) * maxValue);
+
+                _arrSine[i] = sv;
+                _arr[i] = rnd.Next(sv - noise, sv + noise);
+                
+                exp = (int)(_smoothFact * _arr[i] + (1 - _smoothFact) * exp);
+                _arrExp[i] = exp;
+            }
+
+            PanelClear();
+            PanelDrawArray(_penSine, _arrSine);
+            PanelDrawArray(_penArr, _arr);
+            PanelDrawArray(_penExp, _arrExp);
+
+            _gfx.DrawString("Initial Sine", _font, _penSine.Brush, 0, 0);
+            _gfx.DrawString("Sine with noise", _font, _penArr.Brush, 0, _font.GetHeight());
+            _gfx.DrawString("Sine with exponential smoothing", _font, _penExp.Brush, 0, 2 * _font.GetHeight());
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            _smoothFact = trackBar1.Value / 100.0;
+            label1.Text = $"Smoothing factor(0 < a < 1): {_smoothFact:G}";
+            Draw();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            TrackBar1_Scroll(this, EventArgs.Empty);
+        }
+
+        private void Panel1_SizeChanged(object sender, EventArgs e)
+        {
+            TrackBar1_Scroll(this, EventArgs.Empty);
+        }
+    }
+}
